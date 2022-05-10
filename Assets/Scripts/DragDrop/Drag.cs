1116451c70ccb1bdc19d16359849ce6fbbf2471a
Joe Arzua <joearzua@gmail.com>
2022-05-10 19:37:26 -0400

    Initial Drag and Drop feature

--- Assets/Scripts/DragDrop/Drag.cs
+++ Assets/Scripts/DragDrop/Drag.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+// Based on Unity's drag inferface: https://docs.unity3d.com/2019.1/Documentation/ScriptReference/EventSystems.IDragHandler.html
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour
+public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private float speed = 1;
+
+    void Awake()
     {
-        
+        // Get rect transform
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        // anchor drag object to mouse position
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        // Response to beginning drag: Change color of drag object or cursor, sfx, etc.
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        // Response to ending drag: Change color of drag object or cursor, sfx, etc.
     }
 }
