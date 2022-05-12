755c77a11b00738511a83548bd14a518f1cfaebb
Joe Arzua <joearzua@gmail.com>
2022-05-11 23:40:12 -0400

    Updated Drag and Drop functions

--- Assets/Scripts/DragDrop/Drag.cs
+++ Assets/Scripts/DragDrop/Drag.cs
@@ -4,25 +4,51 @@ using UnityEngine.EventSystems;
 
 public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
-    private float speed = 1;
+    private RectTransform dragObject;
+    public CanvasGroup canvasGroup;
+    private Vector3 startPos;
+
+    // For DropArea to let us know object has been dropped
+    private bool dropped;
+    public bool Dropped
+    {
+        get { return dropped; }
+        set { dropped = value; }
+    }
 
     void Awake()
     {
-        // Get rect transform
+        dragObject = transform as RectTransform;
+        startPos = dragObject.position;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         // anchor drag object to mouse position
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(dragObject, eventData.position, eventData.pressEventCamera, out var globalMousePos))
+        {
+            dragObject.position = globalMousePos;
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Response to beginning drag: Change color of drag object or cursor, sfx, etc.
+        // soundManager.PlayDragSFX();
+
+        // Must block raycasts so that DropArea can detect OnDrop from IDropHandler
+        canvasGroup.blocksRaycasts = false;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         // Response to ending drag: Change color of drag object or cursor, sfx, etc.
+        // soundManager.PlayDropSFX();
+
+        // Reset raycasts
+        canvasGroup.blocksRaycasts = true;
+
+        // Reset position if object was not placed in a DropArea
+        if (!Dropped) dragObject.position = startPos;
     }
 }
