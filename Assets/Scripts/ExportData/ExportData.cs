6f6a1b66c7956070841317966ba8a27443f3192b
Joe Arzua <joearzua@gmail.com>
2022-05-12 22:57:34 -0400

    Updated export data feature

---
+++ Assets/Scripts/ExportData/ExportData.cs
@@ -0,0 +1,14 @@
+// Uses System.IO.StreamWriter: https://docs.microsoft.com/en-us/dotnet/api/system.io.streamwriter?view=net-6.0
+using UnityEngine;
+using System.IO;
+
+public class ExportData : MonoBehaviour
+{
+    public void ExportTextFile(string inputText)
+    {
+        // If the file exists, it can be either overwritten or appended to. If the file does not exist, this constructor creates a new file.
+        StreamWriter writer = new StreamWriter(Application.streamingAssetsPath + "/Export Files/Log.txt", true);
+        writer.WriteLine(inputText);
+        writer.Close();
+    }
+}
